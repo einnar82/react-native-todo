dfb0527d20aef377aa1034d3ebfcf19bb4e0eb9d
Rannie Ollit <rannie.ollit@petnet.com.ph>
2020-08-25 15:59:05 +0800

    WIP: Navigation

--- src/views/home.js
+++ src/views/home.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import globalStyles from '../styles/global'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
 
 const Home = ({navigation, ...props}) => {
-    const pressHandler = () => {
-        navigation.navigate('ReviewDetails')
-    }
+    const [reviews, setReviews] = useState([
+        {title: 'Lorem', rating: 5, body: 'ipsum dolor', key: '1'},
+        {title: 'Hey', rating: 4, body: 'hey dolor', key: '2'},
+        {title: 'Dude', rating: 3, body: 'dude dolor', key: '3'},
+    ])
+
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home</Text>
-            <Button
-                title="Go to Review Details"
-                onPress={pressHandler}
+            <FlatList
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
             />
         </View>
     )
--- src/views/reviewDetails.js
+++ src/views/reviewDetails.js
@@ -2,17 +2,12 @@ import React from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import globalStyles from '../styles/global'
 
-const ReviewDetails = ({ navigation, ...props}) => {
-    const backToHomeScreen = () => {
-        navigation.goBack();
-    }
+const ReviewDetails = ({ navigation, route, ...props}) => {
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Review Details</Text>
-            <Button
-                title="Back to Home Screen"
-                onPress={backToHomeScreen}
-            />
+            <Text>{route.params.title}</Text>
+            <Text>{route.params.body}</Text>
+            <Text>{route.params.rating}</Text>
         </View>
     )
 }
